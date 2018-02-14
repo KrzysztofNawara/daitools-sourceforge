40686e665b3da7d2e42fceba3828564582384860
blueeyedhush <blueeyedhush@gmail.com>
2018-02-14 01:00:45 +0100

    Ebx tree processing - core functionality added

--- DAI_Tools/DAI_Tools/EBXExplorer/EbxTreeXmlViewer.cs
+++ DAI_Tools/DAI_Tools/EBXExplorer/EbxTreeXmlViewer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Drawing;
 using System.Data;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -50,13 +51,87 @@ namespace DAI_Tools.EBXExplorer
         }
 
         /**
-         * Processes passed field, attaches new node for that field
+         * Processes passed field, attaches new node for that field (fieldname + DAIComplex name)
          * For each simple value attaches node for it
          * For each complex value simply calls recursively processEbxTree - it'll handle adding node for DAIComplex
          */
         private void processEbxTree(DAIField field, TreeNode parentNode)
         {
+            Debug.Assert(field.ValueType == DAIFieldType.DAI_Complex, "this method should be invoked only for complex fields");
 
+            var treeNode = new TreeNode(formatComplexField(field));
+            parentNode.Nodes.Add(treeNode);
+
+            var value = field.GetComplexValue();
+
+            foreach (var childField in value.Fields)
+            {
+                var fieldName = childField.Descriptor.FieldName;
+                String strValue = null;
+
+                switch (childField.ValueType)
+                {
+                    case DAIFieldType.DAI_Complex:
+                    case DAIFieldType.DAI_Array:
+                        processEbxTree(childField, treeNode);
+                        break;
+                    case DAIFieldType.DAI_String:
+                        strValue = childField.GetStringValue();
+                        break;
+                    case DAIFieldType.DAI_Enum:
+                        strValue = childField.GetEnumValue();
+                        break;
+                    case DAIFieldType.DAI_Int:
+                        strValue = childField.GetIntValue().ToString();
+                        break;
+                    case DAIFieldType.DAI_UInt:
+                        strValue = childField.GetUIntValue().ToString();
+                        break;
+                    case DAIFieldType.DAI_Double:
+                    case DAIFieldType.DAI_Float:
+                        strValue = childField.GetFloatValue().ToString();
+                        break;
+                    case DAIFieldType.DAI_Short:
+                        strValue = childField.GetShortValue().ToString();
+                        break;
+                    case DAIFieldType.DAI_UShort:
+                        strValue = childField.GetUShortValue().ToString();
+                        break;
+                    case DAIFieldType.DAI_Byte:
+                    case DAIFieldType.DAI_UByte:
+                        strValue = childField.GetByteValue().ToString();
+                        break;
+                    case DAIFieldType.DAI_Long:
+                        strValue = childField.GetLongValue().ToString();
+                        break;
+                    case DAIFieldType.DAI_LongLong:
+                        // @ToDo!
+                        strValue = "some long long byte[]...";
+                        break;
+                    case DAIFieldType.DAI_Guid:
+                        // @ToDo!
+                        strValue = "GUID";
+                        break;
+                    case DAIFieldType.DAI_Bool:
+                        strValue = childField.GetBoolValue().ToString();
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+
+                attachSimpleField(fieldName, strValue, treeNode);
+            }
+        }
+
+        private String formatComplexField(DAIField field)
+        {
+            return field.Descriptor.FieldName + " -> " + field.GetComplexValue().Descriptor.FieldName;
+        }
+
+        private void attachSimpleField(String name, String value, TreeNode parent)
+        {
+            var tnode = new TreeNode(name + ": " + value);
+            parent.Nodes.Add(tnode);
         }
     }
 }
