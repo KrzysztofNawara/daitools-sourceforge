6acbf5afcc4d0d85cb98fd16f0fd1d3ba8728385
blueeyedhush <blueeyedhush@gmail.com>
2018-02-14 19:40:26 +0100

    finished conversion logic

--- DAI_Tools/DAI_Tools/Frostbite/EbxAssetContainer.cs
+++ DAI_Tools/DAI_Tools/Frostbite/EbxAssetContainer.cs
@@ -23,13 +23,13 @@ namespace DAI_Tools.Frostbite
         public ValueTypes Type { get; }
     }
 
-    abstract class ASimpleValue : AValue
+    class ASimpleValue : AValue
     {
         public ASimpleValue(String value) : base(ValueTypes.SIMPLE) { this.Val = value; }
         public String Val { get; }
     }
 
-    class ANullRef : AValue { public ANullRef(String v) : base(ValueTypes.NULL_REF) { } }
+    class ANullRef : AValue { public ANullRef() : base(ValueTypes.NULL_REF) { } }
 
     class AIntRef : AValue
     {
@@ -115,10 +115,103 @@ namespace DAI_Tools.Frostbite
             return fakeField;
         }
 
-        private static AValue convert(DAIField root, ConverterContext ctx)
+        private static AValue convert(DAIField field, ConverterContext ctx)
         {
+            AValue result;
             
-            return null;
+            if (field.ValueType == DAIFieldType.DAI_Complex)
+            {
+                var value = field.GetComplexValue();
+                var astruct = new AStruct();
+                astruct.name = value.GetName();
+
+                foreach (var childField in value.Fields)
+                {
+                    AValue convertedChild = convert(childField, ctx);
+                    var childFieldName = childField.Descriptor.FieldName;
+                    astruct.fields.Add(childFieldName, convertedChild);
+                    astruct.correspondingDaiFields.Add(childFieldName, childField);
+                }
+
+                result = astruct;
+            }
+            else if(field.ValueType == DAIFieldType.DAI_Array)
+            {
+                var value = field.GetArrayValue();
+                var aarray = new AArray();
+
+                foreach (var memberField in value.Fields)
+                {
+                    AValue convertedMember = convert(memberField, ctx);
+                    aarray.elements.Add(convertedMember);
+                    aarray.correspondingDaiFields.Add(memberField);
+                }
+
+                result = aarray;
+            }
+            else if (field.ValueType == DAIFieldType.DAI_Guid)
+            {
+                var guid = ctx.file.GetDaiGuidFieldValue(field);
+
+                if (guid.instanceGuid.Equals("null"))
+                    result = new ANullRef();
+                else
+                {
+                    if (guid.external)
+                        result = new AExRef(guid.fileGuid, guid.instanceGuid);
+                    else 
+                        result = new AIntRef(guid.instanceGuid);
+                }
+            }
+            else
+            {
+                String strValue;
+
+                switch (field.ValueType)
+                {
+                    case DAIFieldType.DAI_String:
+                        strValue = field.GetStringValue();
+                        break;
+                    case DAIFieldType.DAI_Enum:
+                        strValue = field.GetEnumValue();
+                        break;
+                    case DAIFieldType.DAI_Int:
+                        strValue = field.GetIntValue().ToString();
+                        break;
+                    case DAIFieldType.DAI_UInt:
+                        strValue = field.GetUIntValue().ToString();
+                        break;
+                    case DAIFieldType.DAI_Double:
+                    case DAIFieldType.DAI_Float:
+                        strValue = field.GetFloatValue().ToString();
+                        break;
+                    case DAIFieldType.DAI_Short:
+                        strValue = field.GetShortValue().ToString();
+                        break;
+                    case DAIFieldType.DAI_UShort:
+                        strValue = field.GetUShortValue().ToString();
+                        break;
+                    case DAIFieldType.DAI_Byte:
+                    case DAIFieldType.DAI_UByte:
+                        strValue = field.GetByteValue().ToString();
+                        break;
+                    case DAIFieldType.DAI_Long:
+                        strValue = field.GetLongValue().ToString();
+                        break;
+                    case DAIFieldType.DAI_LongLong:
+                        strValue = "LL " + DAIEbx.GuidToString(field.GetLongLongValue());
+                        break;
+                    case DAIFieldType.DAI_Bool:
+                        strValue = field.GetBoolValue().ToString();
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+
+                result = new ASimpleValue(strValue);
+            }
+
+            return result;
         }
 
         EbxAssetContainer(String fileGuid, Dictionary<String, AStruct> instances, DAIEbx correspondingEbx)
