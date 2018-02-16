2c61d6a2c8c50afbec9817952b15f0f4cddbffb9
blueeyedhush <blueeyedhush@gmail.com>
2018-02-16 10:37:12 +0100

    adding nuget dependency on Msagl, rendering sample graph in GraphViz form

--- DAI_Tools/DAI_Tools/EBXExplorer/UIGraphAssetViz.cs
+++ DAI_Tools/DAI_Tools/EBXExplorer/UIGraphAssetViz.cs
@@ -17,5 +17,33 @@ namespace DAI_Tools.EBXExplorer
         {
             InitializeComponent();
         }
+
+        private void UIGraphAssetViz_Load(object sender, EventArgs e)
+        {
+            //create a viewer object 
+            Microsoft.Msagl.GraphViewerGdi.GViewer viewer = new Microsoft.Msagl.GraphViewerGdi.GViewer();
+
+            //create a graph object 
+            Microsoft.Msagl.Drawing.Graph graph = new Microsoft.Msagl.Drawing.Graph("graph");
+
+            //create the graph content 
+            graph.AddEdge("A", "B");
+            graph.AddEdge("B", "C");
+            graph.AddEdge("A", "C").Attr.Color = Microsoft.Msagl.Drawing.Color.Green;
+            graph.FindNode("A").Attr.FillColor = Microsoft.Msagl.Drawing.Color.Magenta;
+            graph.FindNode("B").Attr.FillColor = Microsoft.Msagl.Drawing.Color.MistyRose;
+            Microsoft.Msagl.Drawing.Node c = graph.FindNode("C");
+            c.Attr.FillColor = Microsoft.Msagl.Drawing.Color.PaleGreen;
+            c.Attr.Shape = Microsoft.Msagl.Drawing.Shape.Diamond;
+
+            //bind the graph to the viewer 
+            viewer.Graph = graph;
+
+            //associate the viewer with the form 
+            this.SuspendLayout();
+            viewer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Controls.Add(viewer);
+            this.ResumeLayout();
+        }
     }
 }
