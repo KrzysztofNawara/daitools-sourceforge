e26c70d4c58380764eba79adfa963ad615b95095
BlueEyedHush <BlueEyedHush@users.noreply.github.com>
2018-02-17 03:45:31 +0100

    Linking jumpnodes; SplitterNodes removal (#15)
    * drawing edges to jumpnode targets
    
    * special colouring for Instance nodes
    
    * SplitNode removal
    
    * checkbox for enabling/disabling splitter removal
    
    * fixing SplitterNode removal; more airtight exception handling
    
    * working around bug in Msagl (related to SplitterRemoval)

--- DAI_Tools/DAI_Tools/EBXExplorer/UIGraphAssetViz.Designer.cs
+++ DAI_Tools/DAI_Tools/EBXExplorer/UIGraphAssetViz.Designer.cs
@@ -28,20 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.graphVizPanel = new System.Windows.Forms.Panel();
+            this.hideSplittersCheckbox = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(284, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // graphVizPanel
+            // 
+            this.graphVizPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.graphVizPanel.Location = new System.Drawing.Point(0, 25);
+            this.graphVizPanel.Name = "graphVizPanel";
+            this.graphVizPanel.Size = new System.Drawing.Size(284, 237);
+            this.graphVizPanel.TabIndex = 1;
+            // 
+            // hideSplittersCheckbox
+            // 
+            this.hideSplittersCheckbox.AutoSize = true;
+            this.hideSplittersCheckbox.Location = new System.Drawing.Point(13, 7);
+            this.hideSplittersCheckbox.Name = "hideSplittersCheckbox";
+            this.hideSplittersCheckbox.Size = new System.Drawing.Size(114, 17);
+            this.hideSplittersCheckbox.TabIndex = 2;
+            this.hideSplittersCheckbox.Text = "Hide SplitterNodes";
+            this.hideSplittersCheckbox.UseVisualStyleBackColor = true;
+            this.hideSplittersCheckbox.CheckedChanged += new System.EventHandler(this.hideSplittersCheckbox_CheckedChanged);
             // 
             // UIGraphAssetViz
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.hideSplittersCheckbox);
+            this.Controls.Add(this.graphVizPanel);
+            this.Controls.Add(this.toolStrip1);
             this.Name = "UIGraphAssetViz";
             this.Text = "UIGraphAssetViz";
             this.Load += new System.EventHandler(this.UIGraphAssetViz_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.Panel graphVizPanel;
+        private System.Windows.Forms.CheckBox hideSplittersCheckbox;
     }
 }
--- DAI_Tools/DAI_Tools/EBXExplorer/UIGraphAssetViz.cs
+++ DAI_Tools/DAI_Tools/EBXExplorer/UIGraphAssetViz.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using DAI_Tools.Frostbite;
 using Microsoft.Msagl.Drawing;
+using Microsoft.Msagl.GraphViewerGdi;
 using Microsoft.Msagl.Layout.Layered;
 using Microsoft.Msagl.Layout.MDS;
 using Color = Microsoft.Msagl.Drawing.Color;
@@ -26,31 +27,21 @@ namespace DAI_Tools.EBXExplorer
 
         private EbxDataContainers ebxDataContainers;
         private string assetGuid;
+        private GViewer viewer;
 
         private void UIGraphAssetViz_Load(object sender, EventArgs e)
         {
-            try {
-                //create a viewer object 
-                Microsoft.Msagl.GraphViewerGdi.GViewer viewer = new Microsoft.Msagl.GraphViewerGdi.GViewer();
+            //create a viewer object 
+            viewer = new GViewer();
+                
+            //create the graph content 
+            drawGraphSafely();
 
-                //create a graph object 
-                Microsoft.Msagl.Drawing.Graph graph = new Microsoft.Msagl.Drawing.Graph("graph");
-
-                //create the graph content 
-                configureGraph(graph);
-
-                //bind the graph to the viewer 
-                viewer.Graph = graph;
-
-                //associate the viewer with the form 
-                this.SuspendLayout();
-                viewer.Dock = System.Windows.Forms.DockStyle.Fill;
-                this.Controls.Add(viewer);
-                this.ResumeLayout();
-            } catch (Exception ex)
-            {
-                MessageBox.Show("Exception:\n" + ex.Message + "\n" + ex.StackTrace);
-            }
+            //associate the viewer with the form 
+            this.SuspendLayout();
+            viewer.Dock = DockStyle.Fill;
+            this.graphVizPanel.Controls.Add(viewer);
+            this.ResumeLayout();
         }
 
         private class PortDesc
@@ -68,11 +59,28 @@ namespace DAI_Tools.EBXExplorer
             public string nodeLabel;
         }
 
-        private void configureGraph(Graph graph)
+        private void drawGraphSafely()
+        {
+            try {
+                drawGraph();
+            } catch (Exception ex)
+            {
+                MessageBox.Show("Exception:\n" + ex.Message + "\n" + ex.StackTrace);
+            }
+        }
+
+        private void drawGraph()
         {
             var uiGraphAsset = ebxDataContainers.instances[assetGuid];
+            var assetName = uiGraphAsset.data.get("Name").castTo<ASimpleValue>().Val;
+            
+            Graph graph = new Graph(assetName);
+            
             AArray nodes = uiGraphAsset.data.get("Nodes").castTo<AArray>();
+            var splitNodes = new HashSet<string>();
             var portsGuidToPortDesc = new Dictionary<string, PortDesc>();
+            /* target port guid, source vertex name */
+            var jumpNodeEdges = new List<Tuple<string, string>>();
 
             int nodeNextIdx = 0;
             int portNextIdx = 0;
@@ -83,7 +91,8 @@ namespace DAI_Tools.EBXExplorer
                 var nodeAStruct = nodeInRef.refTarget.castTo<AStruct>();
                 var nodeName = nodeAStruct.get("Name").castTo<ASimpleValue>().Val;
                 bool isRoot = Convert.ToBoolean(nodeAStruct.get("IsRootNode").castTo<ASimpleValue>().Val);
-                var nodeType = ebxDataContainers.instances[nodeInRef.instanceGuid].data.name;
+                var nodeContainer = ebxDataContainers.instances[nodeInRef.instanceGuid];
+                var nodeType = nodeContainer.data.name;
                 var nodeLabel = "N" + nodeNextIdx.ToString() + ": " + nodeName + "\n[" + nodeType + "]";
                 nodeNextIdx += 1;
                 var nodeNode = graph.AddNode(nodeLabel);
@@ -100,17 +109,31 @@ namespace DAI_Tools.EBXExplorer
                         portsGuidToPortDesc.Add(dataContainer.guid, portDesc);
                 }
 
+                /* handling special nodes */
+                if (nodeContainer.hasPartial("JumpNode"))
+                {
+                    var targetPortGuid = nodeAStruct.get("TargetPort").castTo<AIntRef>().instanceGuid;
+                    jumpNodeEdges.Add(new Tuple<string, string>(targetPortGuid, nodeLabel));
+                }
+                else if (nodeContainer.hasPartial("SplitterNode"))
+                    splitNodes.Add(nodeLabel);
+
                 /* some visual formatting */
                 nodeNode.Attr.LabelMargin = 3;
                 nodeNode.Attr.Padding = 2;
                 nodeNode.Attr.Shape = Shape.Box;
 
-                if (!isRoot)
-                    nodeNode.Attr.FillColor = Color.LightGreen;
-                else
+                if (isRoot)
                     nodeNode.Attr.FillColor = Color.PaleVioletRed;
+                else if (nodeContainer.hasPartial("InstanceOutputNode"))
+                    nodeNode.Attr.FillColor = Color.LightBlue;
+                else if (nodeContainer.hasPartial("InstanceInputNode"))
+                    nodeNode.Attr.FillColor = Color.MediumPurple;
+                else
+                    nodeNode.Attr.FillColor = Color.LightGreen;
             }
 
+            /* draw ordinary edges */
             var connections = uiGraphAsset.data.get("Connections").castTo<AArray>();
 
             foreach (var connRef in connections.elements)
@@ -125,10 +148,27 @@ namespace DAI_Tools.EBXExplorer
                 if (srcPortDesc.portName.Length > 0 || targetPortDesc.portName.Length > 0)
                     connLabel = srcPortDesc.portName + " -> " + targetPortDesc.portName;
 
-                graph.AddEdge(srcPortDesc.nodeLabel, connLabel, targetPortDesc.nodeLabel);
+                var edge = graph.AddEdge(srcPortDesc.nodeLabel, connLabel, targetPortDesc.nodeLabel);
+                edge.Attr.Weight = 10;
 
                 srcPortDesc.refCount += 1;
                 targetPortDesc.refCount += 1;
+            }
+
+            /* draw special edges */
+            foreach (var jumpEdge in jumpNodeEdges)
+            {
+                var sourceNode = jumpEdge.Item2;
+                var targetPortGuid = jumpEdge.Item1;
+
+                var targetPortDesc = portsGuidToPortDesc[targetPortGuid];
+                var edge = graph.AddEdge(sourceNode, "", targetPortDesc.nodeLabel);
+                targetPortDesc.refCount += 1;
+
+                /* visual formatting */
+                edge.Attr.Weight = 1;
+                edge.Attr.AddStyle(Style.Dotted);
+                edge.Attr.Color = Color.OrangeRed;
             }
 
             foreach (var portDesc in portsGuidToPortDesc.Values)
@@ -148,9 +188,57 @@ namespace DAI_Tools.EBXExplorer
                 }
             }
             
+            this.SuspendLayout();
+            /* bind graph to viewer; must be bound for RemoveNode/Edge to work */
+            viewer.Graph = graph;
+
+            /* remove split nodes */
+            if (hideSplittersCheckbox.Checked)
+            {
+                var nodesToRemove = new List<Node>();
+                var edgesToRemove = new List<Edge>();
+                
+                foreach (var splitNode in splitNodes)
+                {
+                    var node = graph.FindNode(splitNode);
+
+                    foreach (var inEdge in node.InEdges)
+                    {
+                        var inNode = inEdge.SourceNode;
+
+                        foreach (var outEdge in node.OutEdges)
+                        {
+                            var newEdge = graph.AddEdge(inNode.Id, outEdge.LabelText, outEdge.TargetNode.Id);
+                            foreach (var style in outEdge.Attr.Styles)
+                                newEdge.Attr.AddStyle(style);
+                            newEdge.Attr.Color = outEdge.Attr.Color;
+
+                            edgesToRemove.Add(outEdge);
+                        }
+
+                        edgesToRemove.Add(inEdge);
+                    }
+
+                    nodesToRemove.Add(node);
+                }
+
+                foreach (var node in nodesToRemove)
+                    graph.RemoveNode(node);
+            }
+
             /* some visual formatting */
             var layoutSettings = new SugiyamaLayoutSettings();
             graph.LayoutAlgorithmSettings = layoutSettings;
+
+            this.ResumeLayout();
+            /* assign it once again... */
+            viewer.Graph = graph;
+        }
+
+        private void hideSplittersCheckbox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (viewer != null)
+                drawGraphSafely();
         }
     }
 }
