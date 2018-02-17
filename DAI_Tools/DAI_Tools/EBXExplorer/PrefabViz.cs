77586d67d57136ba38d3c5d3b8d8c48498ea9c04
blueeyedhush <blueeyedhush@gmail.com>
2018-02-17 11:16:10 +0100

    BlueprintViz control and button opening it on assets of correct type

---
+++ DAI_Tools/DAI_Tools/EBXExplorer/PrefabViz.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DAI_Tools.Frostbite;
+
+namespace DAI_Tools.EBXExplorer
+{
+    public partial class PrefabViz : Form
+    {
+        private EbxDataContainers ebxContainers;
+        private string assetGuid;
+        
+        public PrefabViz(EbxDataContainers ebxContainers, string assetGuid)
+        {
+            this.ebxContainers = ebxContainers;
+            this.assetGuid = assetGuid;
+            
+            InitializeComponent();
+        }
+    }
+}
