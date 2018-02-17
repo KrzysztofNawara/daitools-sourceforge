09faea28bcca57fb0857c20a6750c732ea16a1d5
blueeyedhush <blueeyedhush@gmail.com>
2018-02-17 23:28:17 +0100

    load on demand - first attempt

--- DAI_Tools/DAI_Tools/EBXExplorer/EbxTreeXmlViewer.Designer.cs
+++ DAI_Tools/DAI_Tools/EBXExplorer/EbxTreeXmlViewer.Designer.cs
@@ -30,10 +30,6 @@
         {
             this.treeView1 = new System.Windows.Forms.TreeView();
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
-            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
-            this.intRefMaxDepth = new System.Windows.Forms.NumericUpDown();
-            this.toolStrip1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.intRefMaxDepth)).BeginInit();
             this.SuspendLayout();
             // 
             // treeView1
@@ -43,48 +39,24 @@
             this.treeView1.Name = "treeView1";
             this.treeView1.Size = new System.Drawing.Size(150, 125);
             this.treeView1.TabIndex = 0;
+            this.treeView1.BeforeExpand += new System.Windows.Forms.TreeViewCancelEventHandler(this.treeView1_BeforeExpand);
             // 
             // toolStrip1
             // 
-            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.toolStripLabel1});
             this.toolStrip1.Location = new System.Drawing.Point(0, 0);
             this.toolStrip1.Name = "toolStrip1";
             this.toolStrip1.Size = new System.Drawing.Size(150, 25);
             this.toolStrip1.TabIndex = 1;
             this.toolStrip1.Text = "toolStrip1";
             // 
-            // toolStripLabel1
-            // 
-            this.toolStripLabel1.Name = "toolStripLabel1";
-            this.toolStripLabel1.Size = new System.Drawing.Size(72, 22);
-            this.toolStripLabel1.Text = "IntRef depth";
-            // 
-            // intRefMaxDepth
-            // 
-            this.intRefMaxDepth.Location = new System.Drawing.Point(86, 3);
-            this.intRefMaxDepth.Name = "intRefMaxDepth";
-            this.intRefMaxDepth.Size = new System.Drawing.Size(45, 20);
-            this.intRefMaxDepth.TabIndex = 2;
-            this.intRefMaxDepth.Value = new decimal(new int[] {
-            5,
-            0,
-            0,
-            0});
-            this.intRefMaxDepth.ValueChanged += new System.EventHandler(this.intRefMaxDepth_ValueChanged);
-            // 
             // EbxTreeXmlViewer
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.intRefMaxDepth);
             this.Controls.Add(this.treeView1);
             this.Controls.Add(this.toolStrip1);
             this.Name = "EbxTreeXmlViewer";
             this.VisibleChanged += new System.EventHandler(this.EbxTreeXmlViewer_VisibleChanged);
-            this.toolStrip1.ResumeLayout(false);
-            this.toolStrip1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.intRefMaxDepth)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -94,7 +66,5 @@
 
         private System.Windows.Forms.TreeView treeView1;
         private System.Windows.Forms.ToolStrip toolStrip1;
-        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
-        private System.Windows.Forms.NumericUpDown intRefMaxDepth;
     }
 }
